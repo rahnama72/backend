46cf3e683e12a7c88d8978636ae79750dbd253c1
mostafa rahnama <m.rah1372@gmail.com>
2020-09-25 18:44:56 +0330

    طراحی مدل پایه
    از این مدل در تمامی مدلهای پروژه ارث برده میشود. این مدل شامل چهار فیلد تاریخ ایجاد ، تاریخ ویرایش، سازنده و آخرین ویراستار میباشد.

--- UserManager/models.py
+++ UserManager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class BasicModel(models.Model):
+    create_by = models.ForeignKey('User', default=1, on_delete=models.SET_DEFAULT,
+                                  verbose_name='سازنده', related_name='create_%(class)ss')
+    update_by = models.ForeignKey('User', default=1, on_delete=models.SET_DEFAULT,
+                                  verbose_name='آخرین ویرایش کننده', related_name='update_%(class)ss')
+
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایش')
+
+    class Meta:
+        abstract = True
+        ordering = ['create_date']
+
+
