756ead783a03484d1952f9afa50f321bb9daa0ff
mostafa rahnama <m.rah1372@gmail.com>
2020-09-25 18:46:25 +0330

    بازنویسی مدل کاربران

--- UserManager/models.py
+++ UserManager/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import UserManager, UnicodeUsernameValidator
 
 
 # Create your models here.
@@ -15,4 +18,39 @@ class BasicModel(models.Model):
         abstract = True
         ordering = ['create_date']
 
+
+class User(AbstractBaseUser, BasicModel):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(max_length=120, unique=True, validators=[username_validator], verbose_name='نام کاربری')
+    activation_code = models.CharField(max_length=120, blank=True, null=True, verbose_name='کد فعال سازی')
+    first_name = models.CharField(verbose_name='نام', max_length=30)
+    last_name = models.CharField(verbose_name='نام خانوادگی', max_length=30)
+    mobile = models.CharField(max_length=11, validators=[RegexValidator(r'^[0-9]{11}$')],
+                              verbose_name='شماره همراه')
+    email = models.EmailField(verbose_name='پست الکترونیکی')
+
+    avatar = models.ImageField(upload_to='users', blank=True, null=True)
+
+    is_active = models.BooleanField(verbose_name='وضعیت', default=True)
+    is_verified = models.BooleanField(verbose_name='کد فعال سازی', default=False)
+    is_staff = models.BooleanField(verbose_name='وضعیت کارمندی', default=False)
+    is_superuser = models.BooleanField(verbose_name='ابر کاربر', default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'mobile', 'email']
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def get_short_name(self):
+        return self.last_name
 
